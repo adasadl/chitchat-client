bcd429097ffc7b9165fa5dccec8f7048855305a2
adasadl <adasadl.praprotnik@gmail.com>
2017-05-31 10:08:45 +0200

    Prijava

--- src/main/java/si/sadl/chitchat/App.java
+++ src/main/java/si/sadl/chitchat/App.java
@@ -1,16 +1,27 @@
-package my.group.chitchat;
+package si.sadl.chitchat;
 
+import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.fluent.Request;
+import org.apache.http.client.utils.URIBuilder;
 
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 /**
  * Hello ChitChat!
  */
 public class App {
     public static void main(String[] args) {
-        try {
-            String hello = Request.Get("http://chitchat.andrej.com")
+    	seznam_uporabnikov();
+    	prijava();
+    	 	
+    }
+    
+    public static void seznam_uporabnikov(){
+    
+    	try {
+            String hello = Request.Get("http://chitchat.andrej.com/users")
                                   .execute()
                                   .returnContent().asString();
             System.out.println(hello);
@@ -18,4 +29,36 @@ public class App {
             e.printStackTrace();
         }
     }
+    
+    public static void prijava(){
+    	
+    	
+    	URI uri;
+    	String responseBody = "";
+		try {
+			uri = new URIBuilder("http://chitchat.andrej.com/users").addParameter("username", "ada").build();
+			responseBody = Request.Post(uri).execute().returnContent().asString();
+		} catch (URISyntaxException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClientProtocolException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	System.out.println(responseBody);
+    	
+    	
+    	
+    	
+    	
+    }
+    
+    
+    
+    
 }
+
+
